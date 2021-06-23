6a9f821cd559f93ee940c74562fda5afdc914846
Middleway13 <middlewayonlinemedia@gmail.com>
2021-06-23 10:54:18 -0400

    multiple files modified

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,8 @@
-from books.models import Book
 from django.contrib import admin
 
+from books.models import User, Book
+
 # Register your models here.
+
+admin.site.register(User)
 admin.site.register(Book)
--- books/views.py
+++ books/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+from .models import Book
 
 # Create your views here.
+
+def homepage(request):
+    # show a homepage
+    return render(request, "books/homepage.html")
+
+@login_required # this is a decorator or function that will redirect you to login page
+def list_books(request):
+    books = Book.objects.all().order_by(created_at)
+    return render(request, "books/books_list.html", {"books": books})
+
